729f112611cdb29641b2cc1107700b12ad8681ea
Diego Alves <diego.oborba@gmail.com>
2026-01-06 02:38:31 -0300

    Add Sobre os Dados to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,9 @@ import streamlit as st
 st.set_page_config(page_title='Logistica Pesada SP', page_icon='🚗', layout='wide')
 
 pg = st.navigation([
-    st.Page("pages/Home.py", title="Home", icon="🏠"),
+    st.Page("pages/Home.py", title="Início", icon="🏠"),
     st.Page("pages/Overview.py", title="Panorama Geral", icon="📊"),
-    st.Page("pages/LogisticsBlackout.py", title="Apagão Logístico", icon="📉")
+    st.Page("pages/LogisticsBlackout.py", title="Apagão Logístico", icon="📉"),
+    st.Page("pages/About_Data.py", title="Sobre os Dados", icon="💾")
 ])
 pg.run()
